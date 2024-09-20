502e4d98731cfb49dbf43d4ecea1f9d632158b9e
Nuramir <nuramirajbynov@gmail.com>
2024-09-20 15:58:39 +0600

    Django project start

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 120, blank = True, null = True)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add = True)
+    authtor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
+
